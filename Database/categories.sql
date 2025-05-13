6d9cbec150168427eeb0c5adfc41197189bf1cb7
Suraj Nagre <84697122+kurai-sx@users.noreply.github.com>
2025-05-13 18:45:32 +0530

    Add files via upload

---
+++ Database/categories.sql
@@ -0,0 +1,12 @@
+SELECT * FROM shopping.categories;
+
+INSERT INTO categories (category_id, name, description) VALUES (1, 'Pass', 'One their here subject law music.');
+INSERT INTO categories (category_id, name, description) VALUES (2, 'Security', 'Data father job country bar over must.');
+INSERT INTO categories (category_id, name, description) VALUES (3, 'Performance', 'Great try right shake few writer.');
+INSERT INTO categories (category_id, name, description) VALUES (4, 'Land', 'So treatment or stay happy.');
+INSERT INTO categories (category_id, name, description) VALUES (5, 'Place', 'Just get happen when.');
+INSERT INTO categories (category_id, name, description) VALUES (6, 'Campaign', 'Wide certain radio bar just medical job think.');
+INSERT INTO categories (category_id, name, description) VALUES (7, 'Section', 'Environmental skin their from.');
+INSERT INTO categories (category_id, name, description) VALUES (8, 'He', 'Main establish go industry bar past tend.');
+INSERT INTO categories (category_id, name, description) VALUES (9, 'Such', 'Size adult peace call.');
+INSERT INTO categories (category_id, name, description) VALUES (10, 'Onto', 'Push line subject executive.');
